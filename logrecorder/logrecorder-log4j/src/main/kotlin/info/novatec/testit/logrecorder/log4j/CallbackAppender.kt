4e75f3548497c23720bd8033560f70c3c23dd589
Stefan Ludwig <stefan.ludwig@novatec-gmbh.de>
2021-10-19 22:23:04 +0200

    copyright notice header for all source files

--- logrecorder/logrecorder-log4j/src/main/kotlin/info/novatec/testit/logrecorder/log4j/CallbackAppender.kt
+++ logrecorder/logrecorder-log4j/src/main/kotlin/info/novatec/testit/logrecorder/log4j/CallbackAppender.kt
@@ -1,12 +1,11 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Copyright 2017-2021 the original author or authors.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,11 +15,7 @@
  */
 package info.novatec.testit.logrecorder.log4j
 
-import org.apache.logging.log4j.core.Appender
-import org.apache.logging.log4j.core.Core
-import org.apache.logging.log4j.core.Filter
-import org.apache.logging.log4j.core.Layout
-import org.apache.logging.log4j.core.LogEvent
+import org.apache.logging.log4j.core.*
 import org.apache.logging.log4j.core.appender.AbstractAppender
 import org.apache.logging.log4j.core.config.plugins.Plugin
 
