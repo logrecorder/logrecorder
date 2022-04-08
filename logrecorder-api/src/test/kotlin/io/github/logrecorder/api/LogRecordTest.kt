2ca2e2b8681d801e4c69871050d23921f9284e89
Stefan Ludwig <stefan.ludwig@novatec-gmbh.de>
2022-04-08 17:25:40 +0200

    optimize Java interoperability for LogRecord interface

--- logrecorder-api/src/test/kotlin/io/github/logrecorder/api/LogRecordTest.kt
+++ logrecorder-api/src/test/kotlin/io/github/logrecorder/api/LogRecordTest.kt
@@ -31,31 +31,37 @@ internal class LogRecordTest {
     val entries4 = LogEntry(logger = "logger-b", level = DEBUG, message = "Message #4", marker = "Marker #4")
     val entries5 = LogEntry(logger = "logger-b", level = INFO, message = "Message #5", marker = "Marker #5")
 
+    @Nested
     @DisplayName("all recorded messages can be got")
-    @Nested inner class Messages {
+    inner class Messages {
 
-        @Test fun `they will be in same order as the entries`() {
+        @Test
+        fun `they will be in same order as the entries`() {
             val cut = logRecord(entries1, entries2, entries3, entries4, entries5)
             assertThat(cut.messages)
                 .containsExactly("Message #1", "Message #2", "Message #3", "Message #4", "Message #5")
         }
 
-        @Test fun `if there are no entries an empty list is returned`() {
+        @Test
+        fun `if there are no entries an empty list is returned`() {
             val cut = logRecord()
             assertThat(cut.messages).isEmpty()
         }
     }
 
+    @Nested
     @DisplayName("all recorded markers can be got")
-    @Nested inner class Markers {
+    inner class Markers {
 
-        @Test fun `they will be in same order as the entries`() {
+        @Test
+        fun `they will be in same order as the entries`() {
             val cut = logRecord(entries1, entries2, entries3, entries4, entries5)
             assertThat(cut.markers)
                 .containsExactly("Marker #1", "Marker #2", "Marker #3", "Marker #4", "Marker #5")
         }
 
-        @Test fun `if there are no entries an empty list is returned`() {
+        @Test
+        fun `if there are no entries an empty list is returned`() {
             val cut = logRecord()
             assertThat(cut.markers).isEmpty()
         }
@@ -69,88 +75,103 @@ internal class LogRecordTest {
         }
     }
 
+    @Nested
     @DisplayName("recorded messages can be got filtered")
-    @Nested inner class FilteredMessages {
+    inner class FilteredMessages {
         val cut = logRecord(entries1, entries2, entries3, entries4, entries5)
 
-        @Test fun `by logger`() {
-            assertThat(cut.messages(logger = "logger-a")).containsExactly("Message #1", "Message #2", "Message #3")
-            assertThat(cut.messages(logger = "logger-b")).containsExactly("Message #4", "Message #5")
-            assertThat(cut.messages(logger = "logger-c")).isEmpty()
+        @Test
+        fun `by logger`() {
+            assertThat(cut.getMessages("logger-a")).containsExactly("Message #1", "Message #2", "Message #3")
+            assertThat(cut.getMessages("logger-b")).containsExactly("Message #4", "Message #5")
+            assertThat(cut.getMessages("logger-c")).isEmpty()
         }
 
-        @Test fun `by level`() {
-            assertThat(cut.messages(level = INFO)).containsExactly("Message #1", "Message #2", "Message #5")
-            assertThat(cut.messages(level = DEBUG)).containsExactly("Message #3", "Message #4")
-            assertThat(cut.messages(level = WARN)).isEmpty()
+        @Test
+        fun `by level`() {
+            assertThat(cut.getMessages(INFO)).containsExactly("Message #1", "Message #2", "Message #5")
+            assertThat(cut.getMessages(DEBUG)).containsExactly("Message #3", "Message #4")
+            assertThat(cut.getMessages(WARN)).isEmpty()
         }
 
-        @Test fun `by logger and level`() {
-            assertThat(cut.messages(logger = "logger-a", level = INFO)).containsExactly("Message #1", "Message #2")
+        @Test
+        fun `by logger and level`() {
+            assertThat(cut.getMessages("logger-a", INFO)).containsExactly("Message #1", "Message #2")
         }
 
     }
 
-
+    @Nested
     @DisplayName("recorded markers can be got filtered")
-    @Nested inner class FilteredMarkers {
+    inner class FilteredMarkers {
 
         val nullMarkerEntry = LogEntry(logger = "logger-d", level = INFO, message = "Nullmarker")
         val cut = logRecord(entries1, entries2, entries3, entries4, entries5, nullMarkerEntry)
 
-        @Test fun `filters entries without markers`() {
-            assertThat(cut.markers(logger = "logger-d")).isEmpty()
+        @Test
+        fun `filters entries without markers`() {
+            assertThat(cut.getMarkers("logger-d")).isEmpty()
         }
 
-        @Test fun `by logger`() {
-            assertThat(cut.markers(logger = "logger-a")).containsExactly("Marker #1", "Marker #2", "Marker #3")
-            assertThat(cut.markers(logger = "logger-b")).containsExactly("Marker #4", "Marker #5")
-            assertThat(cut.markers(logger = "logger-c")).isEmpty()
+        @Test
+        fun `by logger`() {
+            assertThat(cut.getMarkers("logger-a")).containsExactly("Marker #1", "Marker #2", "Marker #3")
+            assertThat(cut.getMarkers("logger-b")).containsExactly("Marker #4", "Marker #5")
+            assertThat(cut.getMarkers("logger-c")).isEmpty()
         }
 
-        @Test fun `by level`() {
-            assertThat(cut.markers(level = INFO)).containsExactly("Marker #1", "Marker #2", "Marker #5")
-            assertThat(cut.markers(level = DEBUG)).containsExactly("Marker #3", "Marker #4")
-            assertThat(cut.markers(level = WARN)).isEmpty()
+        @Test
+        fun `by level`() {
+            assertThat(cut.getMarkers(INFO)).containsExactly("Marker #1", "Marker #2", "Marker #5")
+            assertThat(cut.getMarkers(DEBUG)).containsExactly("Marker #3", "Marker #4")
+            assertThat(cut.getMarkers(WARN)).isEmpty()
         }
 
-        @Test fun `by logger and level`() {
-            assertThat(cut.markers(logger = "logger-a", level = INFO)).containsExactly("Marker #1", "Marker #2")
+        @Test
+        fun `by logger and level`() {
+            assertThat(cut.getMarkers("logger-a", INFO)).containsExactly("Marker #1", "Marker #2")
         }
     }
 
+    @Nested
     @DisplayName("recorded entries can be got filtered")
-    @Nested inner class FilteredEntries {
+    inner class FilteredEntries {
 
         val cut = logRecord(entries1, entries2, entries3, entries4, entries5)
 
-        @Test fun `by logger`() {
-            assertThat(cut.entries(logger = "logger-a")).containsExactly(entries1, entries2, entries3)
-            assertThat(cut.entries(logger = "logger-b")).containsExactly(entries4, entries5)
-            assertThat(cut.entries(logger = "logger-c")).isEmpty()
+        @Test
+        fun `by logger`() {
+            assertThat(cut.getEntries("logger-a")).containsExactly(entries1, entries2, entries3)
+            assertThat(cut.getEntries("logger-b")).containsExactly(entries4, entries5)
+            assertThat(cut.getEntries("logger-c")).isEmpty()
         }
 
-        @Test fun `by level`() {
-            assertThat(cut.entries(level = INFO)).containsExactly(entries1, entries2, entries5)
-            assertThat(cut.entries(level = DEBUG)).containsExactly(entries3, entries4)
-            assertThat(cut.entries(level = WARN)).isEmpty()
+        @Test
+        fun `by level`() {
+            assertThat(cut.getEntries(INFO)).containsExactly(entries1, entries2, entries5)
+            assertThat(cut.getEntries(DEBUG)).containsExactly(entries3, entries4)
+            assertThat(cut.getEntries(WARN)).isEmpty()
         }
 
-        @Test fun `by logger and level`() {
-            assertThat(cut.entries(logger = "logger-a", level = INFO)).containsExactly(entries1, entries2)
+        @Test
+        fun `by logger and level`() {
+            assertThat(cut.getEntries("logger-a", INFO)).containsExactly(entries1, entries2)
         }
 
     }
 
+    @Nested
     @DisplayName("logger names can be generated from classes")
-    @Nested inner class LoggerNames {
+    inner class LoggerNames {
 
-        @Test fun `Kotlin Class`() {
+        @Test
+        fun `Kotlin Class`() {
             val logger = LogRecord.logger(TestLogRecord::class)
             assertThat(logger).isEqualTo("io.github.logrecorder.api.TestLogRecord")
         }
 
-        @Test fun `Java Class`() {
+        @Test
+        fun `Java Class`() {
             val logger = LogRecord.logger(TestLogRecord::class.java)
             assertThat(logger).isEqualTo("io.github.logrecorder.api.TestLogRecord")
         }
