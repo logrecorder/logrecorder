cab7de5af87065b8bd88be9acb3f1639ff6ec84e
sle <sle@novatec-gmbh.de>
2022-09-10 15:35:15 +0200

    Add Logback

--- logrecorder-logback/src/test/kotlin/io/github/logrecorder/logback/kotest/LogbackRecorderKotestExtensionTests.kt
+++ logrecorder-logback/src/test/kotlin/io/github/logrecorder/logback/kotest/LogbackRecorderKotestExtensionTests.kt
@@ -1,59 +1,40 @@
-/*
- * Copyright 2017-2021 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package io.github.logrecorder.logback.junit5
+package io.github.logrecorder.logback.kotest
 
 import io.github.logrecorder.api.LogEntry
 import io.github.logrecorder.api.LogLevel
-import io.github.logrecorder.api.LogRecord
 import io.github.logrecorder.api.LogRecord.Companion.logger
+import io.github.logrecorder.common.kotest.logRecord
 import io.github.logrecorder.logback.util.TestServiceA
 import io.github.logrecorder.logback.util.TestServiceB
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.collections.shouldBeEmpty
+import io.kotest.matchers.collections.shouldContainExactly
 import org.slf4j.LoggerFactory
 import org.slf4j.MDC
 import org.slf4j.helpers.BasicMarkerFactory
 
-internal class LogbackRecorderExtensionTest {
-
+class LogbackRecorderKotestExtensionTests : FunSpec({
     val customLogger = LoggerFactory.getLogger("custom-logger")
 
     val testServiceA = TestServiceA()
     val testServiceB = TestServiceB()
 
-    @BeforeEach
-    fun logSomethingBeforeTest() {
+    beforeEach {
         testServiceA.logSomething()
         testServiceB.logSomething()
     }
 
-    @AfterEach
-    fun cleanup() {
+    afterEach {
         MDC.clear()
     }
 
-    @Test
-    @RecordLoggers(TestServiceA::class, TestServiceB::class, names = ["custom-logger"])
-    fun `extension is registered and log messages are recorded`(log: LogRecord) {
-        assertThat(log.entries).isEmpty()
+    test("log messages are recorded").config(
+        extensions = listOf(recordLogs(TestServiceA::class, TestServiceB::class, names = arrayOf("custom-logger")))
+    ) {
+        logRecord.entries.shouldBeEmpty()
 
         testServiceA.logSomething()
-        assertThat(log.entries).containsExactly(
+        logRecord.entries.shouldContainExactly(
             LogEntry(logger(TestServiceA::class), LogLevel.TRACE, "trace message a", "marker a"),
             LogEntry(logger(TestServiceA::class), LogLevel.DEBUG, "debug message a", "marker a"),
             LogEntry(logger(TestServiceA::class), LogLevel.INFO, "info message a", "marker a"),
@@ -62,7 +43,7 @@ internal class LogbackRecorderExtensionTest {
         )
 
         testServiceB.logSomething()
-        assertThat(log.entries).containsExactly(
+        logRecord.entries.shouldContainExactly(
             LogEntry(logger(TestServiceA::class), LogLevel.TRACE, "trace message a", "marker a"),
             LogEntry(logger(TestServiceA::class), LogLevel.DEBUG, "debug message a", "marker a"),
             LogEntry(logger(TestServiceA::class), LogLevel.INFO, "info message a", "marker a"),
@@ -83,7 +64,7 @@ internal class LogbackRecorderExtensionTest {
         customLogger.warn(marker, "warn message c")
         customLogger.error(marker, "error message c")
 
-        assertThat(log.entries).containsExactly(
+        logRecord.entries.shouldContainExactly(
             LogEntry(logger(TestServiceA::class), LogLevel.TRACE, "trace message a", "marker a"),
             LogEntry(logger(TestServiceA::class), LogLevel.DEBUG, "debug message a", "marker a"),
             LogEntry(logger(TestServiceA::class), LogLevel.INFO, "info message a", "marker a"),
@@ -104,13 +85,11 @@ internal class LogbackRecorderExtensionTest {
         )
     }
 
-    @Test
-    @RecordLoggers(TestServiceA::class)
-    fun `extension is registered and log messages are recorded only for TestServiceA`(log: LogRecord) {
-        assertThat(log.entries).isEmpty()
+    test("log messages are recorded from ServiceA").config(extensions = listOf(recordLogs(TestServiceA::class))) {
+        logRecord.entries.shouldBeEmpty()
 
         testServiceA.logSomething()
-        assertThat(log.entries).containsExactly(
+        logRecord.entries.shouldContainExactly(
             LogEntry(logger(TestServiceA::class), LogLevel.TRACE, "trace message a", "marker a"),
             LogEntry(logger(TestServiceA::class), LogLevel.DEBUG, "debug message a", "marker a"),
             LogEntry(logger(TestServiceA::class), LogLevel.INFO, "info message a", "marker a"),
@@ -119,7 +98,7 @@ internal class LogbackRecorderExtensionTest {
         )
 
         testServiceB.logSomething()
-        assertThat(log.entries).containsExactly(
+        logRecord.entries.shouldContainExactly(
             LogEntry(logger(TestServiceA::class), LogLevel.TRACE, "trace message a", "marker a"),
             LogEntry(logger(TestServiceA::class), LogLevel.DEBUG, "debug message a", "marker a"),
             LogEntry(logger(TestServiceA::class), LogLevel.INFO, "info message a", "marker a"),
@@ -127,13 +106,14 @@ internal class LogbackRecorderExtensionTest {
             LogEntry(logger(TestServiceA::class), LogLevel.ERROR, "error message a", "marker a")
         )
 
-        customLogger.trace("trace message c")
-        customLogger.debug("debug message c")
-        customLogger.info("info message c")
-        customLogger.warn("warn message c")
-        customLogger.error("error message c")
+        val marker = BasicMarkerFactory().getMarker("marker c")
+        customLogger.trace(marker, "trace message c")
+        customLogger.debug(marker, "debug message c")
+        customLogger.info(marker, "info message c")
+        customLogger.warn(marker, "warn message c")
+        customLogger.error(marker, "error message c")
 
-        assertThat(log.entries).containsExactly(
+        logRecord.entries.shouldContainExactly(
             LogEntry(logger(TestServiceA::class), LogLevel.TRACE, "trace message a", "marker a"),
             LogEntry(logger(TestServiceA::class), LogLevel.DEBUG, "debug message a", "marker a"),
             LogEntry(logger(TestServiceA::class), LogLevel.INFO, "info message a", "marker a"),
@@ -142,16 +122,14 @@ internal class LogbackRecorderExtensionTest {
         )
     }
 
-    @Test
-    @RecordLoggers(TestServiceA::class)
-    fun `MDC properties are recorded`(log: LogRecord) {
+    test("MDC properties are recorded").config(extensions = listOf(recordLogs(TestServiceA::class))) {
         MDC.put("custom#1", "foo")
         MDC.put("custom#2", "bar")
         testServiceA.logSingleInfo()
         MDC.remove("custom#2")
         testServiceA.logSingleInfo()
 
-        assertThat(log.entries).containsExactly(
+        logRecord.entries.shouldContainExactly(
             LogEntry(
                 logger = logger(TestServiceA::class),
                 level = LogLevel.INFO,
@@ -174,4 +152,21 @@ internal class LogbackRecorderExtensionTest {
         )
     }
 
-}
+    test("log messages are recorded for String logger").config(extensions = listOf(recordLogs("custom-logger"))) {
+        val marker = BasicMarkerFactory().getMarker("marker c")
+        customLogger.trace(marker, "trace message c")
+        customLogger.debug(marker, "debug message c")
+        customLogger.info(marker, "info message c")
+        customLogger.warn(marker, "warn message c")
+        customLogger.error(marker, "error message c")
+
+        logRecord.entries.shouldContainExactly(
+            LogEntry("custom-logger", LogLevel.TRACE, "trace message c", "marker c"),
+            LogEntry("custom-logger", LogLevel.DEBUG, "debug message c", "marker c"),
+            LogEntry("custom-logger", LogLevel.INFO, "info message c", "marker c"),
+            LogEntry("custom-logger", LogLevel.WARN, "warn message c", "marker c"),
+            LogEntry("custom-logger", LogLevel.ERROR, "error message c", "marker c")
+        )
+    }
+})
+
---
+++ logrecorder-logback/src/test/kotlin/io/github/logrecorder/logback/util/TestFixtures.kt
@@ -0,0 +1,34 @@
+package io.github.logrecorder.logback.util
+
+import org.slf4j.LoggerFactory
+import org.slf4j.helpers.BasicMarkerFactory
+
+class TestServiceA {
+    private val log = LoggerFactory.getLogger(javaClass)
+    private val marker = BasicMarkerFactory().getMarker("marker a")
+
+    fun logSomething() {
+        log.trace(marker, "trace message a")
+        log.debug(marker, "debug message a")
+        log.info(marker, "info message a")
+        log.warn(marker, "warn message a")
+        log.error(marker, "error message a")
+    }
+
+    fun logSingleInfo() {
+        log.info(marker, "info message a")
+    }
+}
+
+class TestServiceB {
+    private val log = LoggerFactory.getLogger(javaClass)
+    private val marker = BasicMarkerFactory().getMarker("marker b")
+
+    fun logSomething() {
+        log.trace(marker, "trace message b")
+        log.debug(marker, "debug message b")
+        log.info(marker, "info message b")
+        log.warn(marker, "warn message b")
+        log.error(marker, "error message b")
+    }
+}
