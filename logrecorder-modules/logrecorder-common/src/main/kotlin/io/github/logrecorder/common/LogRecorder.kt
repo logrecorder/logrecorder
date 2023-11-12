57cc3140952cd03e8957cb8fa58fcd474d66ab60
Stefan Ludwig <stefan.ludwig@novatec-gmbh.de>
2023-11-12 19:58:28 +0100

    add internal hint to common interfaces

--- logrecorder-modules/logrecorder-common/src/main/kotlin/io/github/logrecorder/common/LogRecorder.kt
+++ logrecorder-modules/logrecorder-common/src/main/kotlin/io/github/logrecorder/common/LogRecorder.kt
@@ -1,5 +1,11 @@
 package io.github.logrecorder.common
 
+/**
+ * Internal interface implemented by log-framework modules to allow the starting and stopping of a recording.
+ *
+ * Not intended to be implemented outside the core modules.
+ * We might change this interface at any time.
+ */
 interface LogRecorder {
     fun start()
     fun stop()
--- logrecorder-modules/logrecorder-common/src/main/kotlin/io/github/logrecorder/common/LogRecorderContext.kt
+++ logrecorder-modules/logrecorder-common/src/main/kotlin/io/github/logrecorder/common/LogRecorderContext.kt
@@ -2,6 +2,12 @@ package io.github.logrecorder.common
 
 import io.github.logrecorder.api.LogRecord
 
+/**
+ * Internal interface implemented by log-framework modules to allow the starting and stopping of a recording.
+ *
+ * Not intended to be implemented outside the core modules.
+ * We might change this interface at any time.
+ */
 interface LogRecorderContext {
     val record: LogRecord
     fun start()
